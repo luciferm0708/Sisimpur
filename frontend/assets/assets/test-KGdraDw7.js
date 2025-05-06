7a3f2dcb602eb17f5bf2e88fd5b1dc13479231a1
arpan8925 <sbxp1966@gmail.com>
2025-05-06 20:28:38 +0530

    deleted all

--- frontend/assets/assets/test-KGdraDw7.js
+++
@@ -1,1 +0,0 @@
-function l(o){console.log(`Hello ${o}`)}console.log("Hello world fomr vite");l("Sisimpur Project");
--- frontend/static/js/main.js
+++
@@ -1,5 +0,0 @@
-import '/static/css/main.css'
-import {sayHello} from './important.js'
-
-console.log('Hello world fomr vite')
-sayHello('Sisimpur Project')
